8e5d23ccb903593310c417b98c3ed4255a250cfe
Chirag-2007 <nagpalchirag757@gmail.com>
2025-08-18 22:04:50 +0530

    first commit

---
+++ Day1/Basics.py
@@ -0,0 +1,28 @@
+# Basic Program
+
+print("Hello World!!")
+
+print("Hello World,\nThis is my first code")
+
+'''x = 12
+y = 15
+z = y - x
+print(z)'''
+
+# Variables
+
+#string
+name = "Ram"
+
+#Integer
+roll_number = 17
+
+#Float number
+percentage = 95.8
+
+#Bollean
+is_student = True
+
+print(name, roll_number, percentage, is_student)
+print("My name is " + name + " and my roll number is", roll_number)
+print("I scored", percentage, "% in my final exams. I am a student is", is_student)
