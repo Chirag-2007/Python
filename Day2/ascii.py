e9fa060adc269950f8ba98e40eb229ba160f7808
Chirag-2007 <nagpalchirag757@gmail.com>
2025-08-19 21:45:32 +0530

    Ascii

---
+++ Day2/ascii.py
@@ -0,0 +1,11 @@
+char = "A"
+print(ord(char))
+
+ascii = 67
+print(chr(ascii))
+
+# Taking Input
+name = input("Enter your name : ")
+print(name)
+age = int(input("Enter your age : "))
+print(age)
---
+++ Day2/basic.py
@@ -0,0 +1,12 @@
+# print with seperator
+
+name = "Ram"
+roll_number = 17
+percentage = 95.8
+is_student = True
+print(name, roll_number, percentage, is_student, sep="-")
+
+x=1
+y=2
+z=3
+print(x,y,z,sep="->")
