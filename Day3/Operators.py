e68f64b607eb4c316574aef7b53cdb5959e294fa
Chirag-2007 <nagpalchirag757@gmail.com>
2025-08-20 20:55:39 +0530

    2 Sum and Operators

---
+++ Day3/Operators.py
@@ -0,0 +1,41 @@
+# arithmetic operators
+print("sum:", 4 + 3)
+print("difference:", 4 - 3)
+print("product:", 4*3)
+print("division:", 4 / 3)
+print("floor division:", 4 // 3)
+print("remainder:",4 % 3)
+print("exponentiation:", 4 ** 3)
+
+# assignment operators
+n1 = 5
+n2 = n1
+print(n1, n2)
+n2 *= n1
+print(n1, n2)
+
+# comparision operator
+n1 = 4
+n2 = 3
+print(n1>n2)
+print(n1<n2)
+
+#logical operator
+exp1 = 2 > 1
+exp2 = 5 < 4
+print(" exp1 and exp2:", exp1 and exp2)
+print(" exp1 or exp2:", exp1 or exp2)
+print(" not exp1:", not(exp1))
+
+# Identity operators
+x = 5
+y = 5
+print(" if x is y:", x is y)
+print(" if x is not  y:", x is not y)
+
+#Membership operator
+fruits = ["apple", "banana", "cherry"]
+print("if banana is present in fruits:", "banana" in fruits)
+print("if mango is not present in fruits:", "mango" not in fruits)
+
+
