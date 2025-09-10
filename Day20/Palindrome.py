2c38f26b6fadeea45096e68fe4adb9ae6edfb016
Chirag-2007 <nagpalchirag757@gmail.com>
2025-09-10 20:09:42 +0530

    Questions Practice

---
+++ Day20/Palindrome.py
@@ -0,0 +1,11 @@
+def check_palindrome(str):
+
+    clean_str = (str.replace(" ","")).lower()
+    reverse_str = clean_str[::-1]
+    return clean_str == reverse_str
+
+str = input("Enter a string: ")
+if check_palindrome(str):
+    print("It is a palindrome string")
+else:
+    print("Not a palindrome string")
